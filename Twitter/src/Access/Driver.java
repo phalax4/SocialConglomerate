755d5b0a32e4e9075fc04ec8792398bf7520096b
BrokenRecollect <jessica.shen97@gmail.com>
2013-07-18 15:12:29 -0400

    Twitter "final"

---
+++ Twitter/src/Access/Driver.java
@@ -0,0 +1,17 @@
+package Access;
+
+
+public class Driver {
+	
+	public static void main(String args[]) throws Exception {
+		
+
+		Tweet dave = new Tweet();
+		System.out.println(dave.check());
+
+		
+	
+
+	}
+	
+}
